b3c554e87ff14d4c483d4d6f14ffa6ef17ceab34
Jayden Seric <me@jaydenseric.com>
2021-01-21 14:41:40 +1100

    Internal JSDoc tweak.

--- private/errorConsole.js
+++ private/errorConsole.js
@@ -5,7 +5,7 @@ const { Console } = require('console');
 /**
  * The `console` API, but all output is to `stderr`. This allows `console.group`
  * to be used with `console.error`.
- * @kind constant
+ * @kind member
  * @name errorConsole
  * @ignore
  */
