104a27d5e48d9e2bd34e93cbeedfc0a0c26cda3e
Jayden Seric <me@jaydenseric.com>
2021-01-22 17:24:01 +1100

    Improved CLI argument related errors.

--- cli/jsdoc-md.js
+++ cli/jsdoc-md.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const arg = require('arg');
+const CliError = require('../private/CliError');
 const reportCliError = require('../private/reportCliError');
 const jsdocMd = require('../public/jsdocMd');
 
@@ -15,22 +16,70 @@ const jsdocMd = require('../public/jsdocMd');
  */
 async function jsdocMdCli() {
   try {
+    const { _: unexpectedArgs, ...expectedArgs } = arg(
+      {
+        '--source-glob': String,
+        '-s': '--source-glob',
+        '--markdown-path': String,
+        '-m': '--markdown-path',
+        '--target-heading': String,
+        '-t': '--target-heading',
+      },
+      {
+        // Don’t throw on unexpected arguments.
+        permissive: true,
+      }
+    );
+
+    if (unexpectedArgs.length)
+      throw new CliError(
+        `Command \`jsdoc-md\` unexpected argument${
+          unexpectedArgs.length === 1 ? '' : 's'
+        } \`${unexpectedArgs.join('`, `')}\`.`
+      );
+
+    for (const [argName, argValue] of Object.entries(expectedArgs))
+      if (typeof argValue === 'string' && !argValue)
+        throw new CliError(
+          `Command \`jsdoc-md\` argument \`${argName}\` requires a value.`
+        );
+
     const {
       '--source-glob': sourceGlob,
       '--markdown-path': markdownPath,
       '--target-heading': targetHeading,
-    } = arg({
-      '--source-glob': String,
-      '-s': '--source-glob',
-      '--markdown-path': String,
-      '-m': '--markdown-path',
-      '--target-heading': String,
-      '-t': '--target-heading',
-    });
+    } = expectedArgs;
 
     await jsdocMd({ sourceGlob, markdownPath, targetHeading });
   } catch (error) {
-    reportCliError('jsdoc-md', error);
+    let reportError = error;
+
+    if (error instanceof arg.ArgError)
+      switch (error.code) {
+        case 'ARG_MISSING_REQUIRED_SHORTARG': {
+          const [, badArgName] = error.message.match(/: (.+?)$/u);
+
+          reportError = new CliError(
+            `Command \`jsdoc-md\` argument \`${badArgName}\` requires a value, but was followed by another short argument.`
+          );
+
+          break;
+        }
+
+        case 'ARG_MISSING_REQUIRED_LONGARG': {
+          const [, badArgName, aliasForArgName] = error.message.match(
+            /: (.+?)(?: \(alias for (.+?)\))?$/u
+          );
+
+          reportError = new CliError(
+            `Command \`jsdoc-md\` argument \`${badArgName}\`${
+              aliasForArgName ? ` (alias for \`${aliasForArgName}\`)` : ''
+            } requires a value.`
+          );
+        }
+      }
+
+    reportCliError('jsdoc-md', reportError);
 
     process.exitCode = 1;
   }
--- test/cli/jsdoc-md.test.js
+++ test/cli/jsdoc-md.test.js
@@ -10,6 +10,453 @@ const snapshot = require('snapshot-assertion');
 const cliPath = resolve(__dirname, '../../cli/jsdoc-md');
 
 module.exports = (tests) => {
+  tests.add(
+    '`jsdoc-md` CLI with argument `--source-glob` value empty.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--source-glob='],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-source-glob-value-empty-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `--source-glob` value missing.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--source-glob'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-source-glob-value-missing-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add('`jsdoc-md` CLI with argument `-s` value missing.', async () => {
+    await disposableDirectory(async (tempDirPath) => {
+      const { stdout, stderr, status, error } = spawnSync(
+        'node',
+        [cliPath, '-s'],
+        {
+          cwd: tempDirPath,
+          env: {
+            ...process.env,
+            FORCE_COLOR: 1,
+          },
+        }
+      );
+
+      if (error) throw error;
+
+      strictEqual(stdout.toString(), '');
+
+      await snapshot(
+        stderr.toString(),
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/argument-s-value-missing-stderr.ans'
+        )
+      );
+
+      strictEqual(status, 1);
+    });
+  });
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `-s` value missing due to following short arg.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '-ss'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-s-value-missing-following-short-arg-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `--markdown-path` value empty.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--markdown-path='],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-markdown-path-value-empty-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `--markdown-path` value missing.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--markdown-path'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-markdown-path-value-missing-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add('`jsdoc-md` CLI with argument `-m` value missing.', async () => {
+    await disposableDirectory(async (tempDirPath) => {
+      const { stdout, stderr, status, error } = spawnSync(
+        'node',
+        [cliPath, '-m'],
+        {
+          cwd: tempDirPath,
+          env: {
+            ...process.env,
+            FORCE_COLOR: 1,
+          },
+        }
+      );
+
+      if (error) throw error;
+
+      strictEqual(stdout.toString(), '');
+
+      await snapshot(
+        stderr.toString(),
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/argument-m-value-missing-stderr.ans'
+        )
+      );
+
+      strictEqual(status, 1);
+    });
+  });
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `-m` value missing due to following short arg.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '-mm'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-m-value-missing-following-short-arg-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `--target-heading` value empty.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--target-heading='],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-target-heading-value-empty-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `--target-heading` value missing.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '--target-heading'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-target-heading-value-missing-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add('`jsdoc-md` CLI with argument `-t` value missing.', async () => {
+    await disposableDirectory(async (tempDirPath) => {
+      const { stdout, stderr, status, error } = spawnSync(
+        'node',
+        [cliPath, '-t'],
+        {
+          cwd: tempDirPath,
+          env: {
+            ...process.env,
+            FORCE_COLOR: 1,
+          },
+        }
+      );
+
+      if (error) throw error;
+
+      strictEqual(stdout.toString(), '');
+
+      await snapshot(
+        stderr.toString(),
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/argument-t-value-missing-stderr.ans'
+        )
+      );
+
+      strictEqual(status, 1);
+    });
+  });
+
+  tests.add(
+    '`jsdoc-md` CLI with argument `-t` value missing due to following short arg.',
+    async () => {
+      await disposableDirectory(async (tempDirPath) => {
+        const { stdout, stderr, status, error } = spawnSync(
+          'node',
+          [cliPath, '-mm'],
+          {
+            cwd: tempDirPath,
+            env: {
+              ...process.env,
+              FORCE_COLOR: 1,
+            },
+          }
+        );
+
+        if (error) throw error;
+
+        strictEqual(stdout.toString(), '');
+
+        await snapshot(
+          stderr.toString(),
+          resolve(
+            __dirname,
+            '../snapshots/jsdoc-md/argument-t-value-missing-following-short-arg-stderr.ans'
+          )
+        );
+
+        strictEqual(status, 1);
+      });
+    }
+  );
+
+  tests.add('`jsdoc-md` CLI with an unexpected argument.', async () => {
+    await disposableDirectory(async (tempDirPath) => {
+      const { stdout, stderr, status, error } = spawnSync(
+        'node',
+        [cliPath, '--unexpected-a'],
+        {
+          cwd: tempDirPath,
+          env: {
+            ...process.env,
+            FORCE_COLOR: 1,
+          },
+        }
+      );
+
+      if (error) throw error;
+
+      strictEqual(stdout.toString(), '');
+
+      await snapshot(
+        stderr.toString(),
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/unexpected-argument-stderr.ans'
+        )
+      );
+
+      strictEqual(status, 1);
+    });
+  });
+
+  tests.add('`jsdoc-md` CLI with unexpected arguments.', async () => {
+    await disposableDirectory(async (tempDirPath) => {
+      const { stdout, stderr, status, error } = spawnSync(
+        'node',
+        [cliPath, '--unexpected-a', '--unexpected-b=', '--unexpected-c=abc'],
+        {
+          cwd: tempDirPath,
+          env: {
+            ...process.env,
+            FORCE_COLOR: 1,
+          },
+        }
+      );
+
+      if (error) throw error;
+
+      strictEqual(stdout.toString(), '');
+
+      await snapshot(
+        stderr.toString(),
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/unexpected-arguments-stderr.ans'
+        )
+      );
+
+      strictEqual(status, 1);
+    });
+  });
+
   tests.add('`jsdoc-md` CLI with defaults.', async () => {
     await disposableDirectory(async (tempDirPath) => {
       const fileNameSourceIgnored = 'C.js';
@@ -160,7 +607,7 @@ export default 'B';
     });
   });
 
-  tests.add('`jsdoc-md` CLI with an error.', async () => {
+  tests.add('`jsdoc-md` CLI with an invalid JSdoc error.', async () => {
     await disposableDirectory(async (tempDirPath) => {
       const pathMd = join(tempDirPath, 'readme.md');
       const pathSource = join(tempDirPath, 'A.js');
@@ -191,7 +638,10 @@ export default 'B';
 
       await snapshot(
         stderr.toString(),
-        resolve(__dirname, '../snapshots/jsdoc-md/error-stderr.ans')
+        resolve(
+          __dirname,
+          '../snapshots/jsdoc-md/invalid-jsdoc-error-stderr.ans'
+        )
       );
 
       strictEqual(status, 1);
